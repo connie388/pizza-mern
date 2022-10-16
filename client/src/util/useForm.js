9e55ab1c867870d43f750df048ebcfde4bd35497
Connie <sitwailan@gmail.com>
2022-10-15 20:49:47 -0400

    fixed bug

--- client/src/util/useForm.js
+++ client/src/util/useForm.js
@@ -160,7 +160,10 @@ const useForm = (initialJson, callback) => {
     event.persist();
 
     const target = event.target;
-    const value = target.type === "checkbox" ? target.checked : target.value;
+    const value =
+      target.getAttribute("type") === "checkbox"
+        ? target.checked
+        : target.value;
     const name = target.name;
     validate(event, name, value);
 
